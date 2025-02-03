d6d57624bcddab5abede7ac7b06837527bb7ed7f
Supernedo <che.salazar@my.uwi.edu>
2025-02-02 22:14:58 -0400

    Added QR Code Reading on Update function

--- Assets/Scripts/QRCodeProcessing.cs
+++ Assets/Scripts/QRCodeProcessing.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -23,26 +24,50 @@ public class QRCodeProcessing : MonoBehaviour
         webCamTexture.Play();
     }
 
-    //private IEnumerator InitializeCamera()
-    //{
-    //    // Wait to avoid potential camera crashes
-    //    yield return new WaitForSeconds(1.25f);
-    //}
-
     void Update()
     {
-        IBarcodeReader reader = new BarcodeReader();
-
-        var result = reader.Decode(webCamTexture.GetPixels32(), webCamTexture.width, webCamTexture.height);
-        if (result != null)
+        if (webCamTexture.width > 100) // This is just a way to determine if the camera is initializaed
         {
-            text.text = result.Text;
-            webCamTexture.Stop();
-        }
+            IBarcodeReader reader = new BarcodeReader();
 
-        //if (webCamTexture.width > 100)
-        //{
-            
-        //}
+            UpdateCameraDisplay();
+            AdjustAspectRatio();
+
+            var result = reader.Decode(webCamTexture.GetPixels32(), image.texture.width, image.texture.height);
+            if (result != null)
+            {
+                text.text = result.Text;
+
+                // Do important stuff here or something
+            }
+        }
+    }
+
+    private void UpdateCameraDisplay()
+    {
+        // Get camera rotation and adjust UI accordingly
+        int rotationAngle = -webCamTexture.videoRotationAngle;
+
+        // If someone manages to get the device upside down...
+        if (webCamTexture.videoVerticallyMirrored)
+            rotationAngle += 180; 
+
+        image.rectTransform.localEulerAngles = new Vector3(0, 0, rotationAngle);
+    }
+
+    void AdjustAspectRatio()
+    {
+        float cameraAspect = (float)webCamTexture.width / webCamTexture.height;
+        float screenAspect = (float)Screen.width / Screen.height;
+
+        if (cameraAspect > screenAspect)
+            image.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.width / cameraAspect);
+        else
+            image.rectTransform.sizeDelta = new Vector2(Screen.height * cameraAspect, Screen.height);
+    }
+
+    private void DisableReader()
+    {
+        webCamTexture.Stop();
     }
 }
